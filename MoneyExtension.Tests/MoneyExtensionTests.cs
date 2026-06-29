ba92169f7adc51c7ac195384c4a561b5752cb2ad
Caio Holanda <caiojholanda@gmail.com>
2026-06-29 16:09:18 +0100

    Finalizado a primeira versao

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShoulConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
